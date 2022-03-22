20336516590be4e311456f44223022f27ebd6103
P.angelino <p.angelinodaniele@gmail.com>
2022-03-22 13:49:50 +0100

    Esercizio finito

---
+++ main.js
@@ -0,0 +1,25 @@
+let numeroChilometri = prompt("inserisci il numero di chilometri");
+let età = prompt("Inserisci l'età");
+let prezzo;
+
+
+parseFloat(numeroChilometri);
+totale = numeroChilometri * 0.21;
+
+if (età < 18){
+    let sconto;
+    sconto = (totale / 100)*20
+    totale = totale - sconto;
+
+    
+}else if(età > 64){
+
+    sconto = (totale / 100)*40
+    totale = totale - sconto;
+}
+
+
+
+totale=totale.toFixed(2);
+
+document.getElementById("prezzo").innerHTML = totale;
